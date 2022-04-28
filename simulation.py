35a2b2a61393ad7c549467f12f9b3406e87371a0
Nicholas Trindade <nmd.trindade@gmail.com>
2022-04-28 14:01:17 -0700

    fixed shortest path determination process

--- simulation.py
+++ simulation.py
@@ -2,6 +2,23 @@ from cell import Cell
 from map_constants import *
 
 
+def get_cardinal_moves(coordinate):
+    column, row = coordinate
+    north = (column, row - 1)
+    south = (column, row + 1)
+    west = (column - 1, row)
+    east = (column + 1, row)
+    return [north, south, east, west]
+
+
+
+
+def get_manhattan_distance(start, finish) -> int:
+    row1, column1 = start
+    row2, colum2 = finish
+    return abs(row1 - row2) + abs(column1 - colum2)
+
+
 class AStarSearch:
 
     def __init__(self, map):
@@ -18,6 +35,7 @@ class AStarSearch:
         open = [self.get_cell(self.start)]
         closed = []
         visited = set()
+        visited.add(self.start)
         while len(open) > 0:
             q = open[0]
             for cell in open:
@@ -34,31 +52,19 @@ class AStarSearch:
                     open.append(successor)
         return closed
 
-    def get_shortest_path(self, evaluated_cells) -> list:
-        def is_cardinal_move() -> bool:
-            x, y = shortest_path[-1].coordinate
-            north = (x, y - 1)
-            south = (x, y + 1)
-            west = (x - 1, y)
-            east = (x + 1, y)
-            if cell.coordinate in [north, south, east, west]:
-                return True
-            else:
-                return False
-
-        evaluated_cells = sorted(evaluated_cells, key=lambda cell: cell.g, reverse=True)
-        shortest_path = [evaluated_cells[0]]
-        for cell in evaluated_cells:
-            if cell.g < shortest_path[-1].g and is_cardinal_move():
-                shortest_path.append(cell)
-        return reversed(shortest_path)
+    def get_successors(self, coordinate) -> list:
+        cardinal_moves = get_cardinal_moves(coordinate)
+        for move in list(cardinal_moves):
+            if not self.is_valid_move(move):
+                cardinal_moves.remove(move)
+        return cardinal_moves
 
     def get_cell(self, coordinate, q=None) -> Cell:
         if q:
             g = q.g + self.get_movement_cost(coordinate)
         else:
             g = 0
-        h = self.get_manhattan_distance(coordinate, self.goal)
+        h = get_manhattan_distance(coordinate, self.goal)
         f = g + h
         return Cell(coordinate, g, h, f)
 
@@ -68,18 +74,6 @@ class AStarSearch:
             return 2
         elif self.map[coordinate] == SPACE:
             return 1
-
-    def get_successors(self, coordinate) -> list:
-        x, y = coordinate
-        north = (x, y - 1)
-        south = (x, y + 1)
-        west = (x - 1, y)
-        east = (x + 1, y)
-        cardinal_moves = [north, south, west, east]
-        for move in list(cardinal_moves):
-            if not self.is_valid_move(move):
-                cardinal_moves.remove(move)
-        return cardinal_moves
 
     def is_valid_move(self, coordinate) -> bool:
         if 0 <= coordinate[0] < self.map_width and 0 <= coordinate[1] < self.map_height:
@@ -97,11 +91,27 @@ class AStarSearch:
         else:
             return False
 
-    def get_manhattan_distance(self, start, finish) -> int:
-        return abs(start[0] - finish[0]) + abs(start[1] - finish[1])
+    def get_shortest_path(self, evaluated_cells) -> list:
+        def is_cardinal_move() -> bool:
+            if cell.coordinate in get_cardinal_moves(shortest_path[-1].coordinate):
+                return True
+            else:
+                return False
+
+        evaluated_cells = sorted(evaluated_cells, key=lambda cell: cell.g, reverse=True)
+        shortest_path = [evaluated_cells[0]]
+        for cell in evaluated_cells:
+            print(cell.coordinate)
+        for cell in evaluated_cells:
+            if cell.g < shortest_path[-1].g and is_cardinal_move():
+                shortest_path.append(cell)
+            if cell.coordinate == self.start:
+                break
+        return reversed(shortest_path)
+
 
     def get_two_to_one_dimensional_coordinates(self, coordinates) -> int:
-        column,row = coordinates
+        column, row = coordinates
         return column + row * self.map_width
 
     def get_one_to_two_dimensional_coordinates(self, coordinates) -> list:
